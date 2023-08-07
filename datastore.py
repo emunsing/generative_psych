093b581c19caae985613cf655ded5af1ddbad3da
emunsing <eric.munsing@gmail.com>
2023-08-06 20:35:53 -0700

    Refactor filestore, complete tests

--- datastore.py
+++ datastore.py
@@ -5,7 +5,7 @@ import hashlib
 import abc
 
 
-class NarrativeStore(abc.ABCMeta):
+class NarrativeStore(abc.ABC):
     """
     Writer affordances: 
     - Create narrative record
@@ -28,38 +28,41 @@ class NarrativeStore(abc.ABCMeta):
     """
     
     @abc.abstractmethod
-    def create_conversation_record():
+    def create_conversation_record(self, narrative_uid):
         # Should also update the conversation structure model if needed
         pass
 
     @abc.abstractmethod
-    def append_conversation_record():
+    def append_conversation_record(self):
         pass
 
     @abc.abstractmethod
-    def get_conversation_record(narrative_index, conversation_index):
+    def get_conversation_record(self, narrative_index, conversation_index):
         pass
 
     @abc.abstractmethod
-    def get_next_conversation_index(current_narrative_index, current_conversation_index):
+    def get_next_conversation_index(self, current_narrative_index, current_conversation_index):
         pass
     
     @abc.abstractmethod
-    def save_reader_position(narrative_index, conversation_index):
+    def save_reader_position(self, narrative_index, conversation_index):
         pass
 
     @abc.abstractmethod
-    def compute_conversation_distance(nidx1, cidx1, nidx2, cidx2):
+    def count_unread_conversations(self):
         pass
 
 
-class FileNarrativeStore():
+class FileNarrativeStore(NarrativeStore):
     """
-    
+    - Each narrative stored in a different folder, numbered sequentially
+    - Each conversation stored in a different file, numbered sequentially
+    - Dict Mapping of Narrative UIDs to folder indices in self.uid_index_fpath
+    - Numeric tree structure as dict in self.narrative_index_fpath
     """
     def __init__(self, data_dir=None):
         self.data_dir = data_dir
-        self.narrative_dir_format = '{idx}_{uid}'
+        self.narrative_dir_format = '{idx}'
         self.conversation_file_format = '{idx}.txt'
         self.narrative_index_fpath = os.path.join(self.data_dir, 'narrative_index.json')
         self.reader_position_fpath = os.path.join(self.data_dir, 'reader_position.json')
@@ -78,95 +81,87 @@ class FileNarrativeStore():
             with open(self.uid_index_fpath, 'w') as f:
                 json.dump({}, f)
 
-
-    def head_narrative_handler(self, narrative_uid:[str, None]=None):
-        narrative_dirs = os.walk(self.data_dir).__next__()[1]
-        if len(narrative_dirs) > 0:
-            dir_tups = [d.split("_") for d in narrative_dirs]
-            dir_tups = sorted(dir_tups, key=lambda t: t[0])
-            dirs_matching_uid = [t for t in dir_tups if t[1] == narrative_uid]
-            most_recent_index, most_recent_hash = dir_tups[-1]
-            if narrative_uid is None:
-                # Main exit point for simple query, returning the most recent directory
-                return self.narrative_dir_format.format(idx=most_recent_index, uid=most_recent_hash)
-            else:
-                assert len(dirs_matching_uid) == 0 or most_recent_hash == narrative_uid, "UID exists in a directory that is not the most recent"
-        else:
-            assert narrative_uid is not None, "No current directories and no narrative UID provided"
-            most_recent_index, most_recent_hash = 0, ''
-
-        if most_recent_hash == narrative_uid:
-            return self.narrative_dir_format.format(idx=most_recent_index, uid=most_recent_hash)
-        else:
-            novel_idx = most_recent_index+1
-            with open(self.uid_index_fpath, 'r') as f:
-                uid_index_map = json.load(f)
-            uid_index_map[narrative_uid] = novel_idx
-            with open(self.uid_index_fpath, 'w') as f:
-                json.dump(uid_index_map, f)
-
-            dirname = self.narrative_dir_format.format(idx=novel_idx, uid=narrative_uid)
-            os.makedirs(os.path.join(self.data_dir, dirname))
-            return dirname  
-
-    def make_head_narrative_dir(self, narrative_uid:str):
-        # If the narrative uid is not the most recent narrative, create a new directory
-        # Raise an error if the narrative_uid is already in the narrative list, but not the most recent
-        # return the narrative directory
-        return self.head_narrative_handler(narrative_uid)
-        
-    def get_head_narrative_dir(self):
-        return self.head_narrative_handler()
+    def get_uid_index(self):
+        with open(self.uid_index_fpath, 'r') as f:
+            return json.load(f)
     
-    def get_narrative_dir(self, narrative_uid:str=None):
-        # Get a specific narrative directory's path, or the head path if narrative_uid=None
-        if narrative_uid is None:
-            return self.get_head_narrative_dir()
-        narrative_dirs = os.walk(self.data_dir).__next__()[1]
-        assert len(narrative_dirs) > 0, "No narrative directories found"
-        match_dir = [d for d in narrative_dirs if narrative_uid in d][0]
-        return match_dir
+    def get_narrative_tree(self):
+        with open(self.narrative_index_fpath, 'r') as f:
+            return json.load(f)
+        
+    def get_head_narrative_idx(self):
+        uid_index = self.get_uid_index()
+        return max(uid_index.values())
 
-    def get_head_conversation_index(self, narrative_uid:str=None):
-        narrative_dir = self.get_narrative_dir(narrative_uid)
-        conversation_files = os.walk(os.path.join(self.data_dir, narrative_dir)).__next__()[2]
-        if len(conversation_files) > 0:
-            current_conversation_index = max([int(f.split(".")[0]) for f in conversation_files])
-            return current_conversation_index
-        else:
-            return 0
+    def get_set_narrative_idx(self, narrative_uid:str=None):
+        if narrative_uid is None:
+            return self.get_head_narrative_idx()
+        uid_index = self.get_uid_index()
+        idx = uid_index.get(narrative_uid, len(uid_index))
+        uid_index[narrative_uid] = idx
+        with open(self.uid_index_fpath, 'w') as f:
+            json.dump(uid_index, f)
+
+        full_dir_path = os.path.join(self.data_dir, self.narrative_dir_format.format(idx=idx))
+        if not os.path.exists(full_dir_path):
+            os.mkdir(full_dir_path)        
+        return idx
+    
+    def format_fpath(self, narrative_idx, conversation_idx):
+        return os.path.join(self.data_dir, 
+                            self.narrative_dir_format.format(idx=narrative_idx),
+                            self.conversation_file_format.format(idx=conversation_idx))
 
     def get_conversation_fpath(self, narrative_uid:str=None, conversation_index:int=None):
         # If narrative_uid is None, look in the most recent narrative
         # If conversation_index is None, return the most recent conversation
-        narrative_dir = self.get_narrative_dir(narrative_uid)
+        narrative_idx = self.get_set_narrative_idx(narrative_uid)
+
         if conversation_index is None:
-            conversation_index = self.get_head_conversation_index(narrative_uid)
-        return os.path.join(self.data_dir, narrative_dir, self.conversation_file_format.format(idx=conversation_index))
+            # Get the head index from the 
+            narrative_tree = self.get_narrative_tree()
+            conversation_index = narrative_tree[str(narrative_idx)][-1]
+        return self.format_fpath(narrative_idx, conversation_index)
 
     def create_conversation_record(self, narrative_uid):
-        narrative_dir = self.make_head_narrative_dir(narrative_uid)
-        current_conversation_index = self.get_head_conversation_index(narrative_uid)
-        with open(self.get_conversation_fpath(narrative_uid), 'w') as f:
-            pass
-        with open(self.narrative_index_fpath, 'r') as f:
-            narrative_tree = json.load(f)
-        narrative_idx = narrative_dir.split("_")[0]
-        narrative_tree[narrative_idx] = current_conversation_index + 1
+        narrative_idx = self.get_set_narrative_idx(narrative_uid) # Force creation of a new directory
+        narrative_tree = self.get_narrative_tree()
+        current_conversation_list = narrative_tree.get(str(narrative_idx), [])
+        new_conversation_index = len(current_conversation_list)
+        narrative_tree[str(narrative_idx)] = current_conversation_list + [new_conversation_index]
         with open(self.narrative_index_fpath, 'w') as f:
             json.dump(narrative_tree, f)
+        with open(self.format_fpath(narrative_idx, new_conversation_index), 'w') as f:
+            pass
+        return 
 
     def append_conversation_record(self, lines:list):
+        # Get the most recent conversation
         with open(self.get_conversation_fpath(), 'a') as f:
             for line in lines:
                 f.write(f'{line}\n')
         
+    def get_conversation_record(self, narrative_index, conversation_index):
+        conversation_fpath = self.format_fpath(narrative_index, conversation_index)
+        with open(conversation_fpath, 'r') as f:
+            return f.read()
+
     def count_unread_conversations(self):
-        with open(self.narrative_index_fpath, 'r') as f:
-            narrative_tree = json.load(f)
+        narrative_tree = self.get_narrative_tree()
         with open(self.reader_position_fpath, 'r') as f:
             reader_pos = json.load(f)
-        remaining_chapters = narrative_tree[reader_pos[0]] - reader_pos[1]
+        remaining_chapters = narrative_tree[str(reader_pos[0])][-1] - reader_pos[1]
         for narrative_idx in range(reader_pos[0]+1, len(narrative_tree)):
-            remaining_chapters += narrative_tree[narrative_idx]
+            remaining_chapters += len(narrative_tree[str(narrative_idx)])
         return remaining_chapters
+
+    def get_next_conversation_index(self, current_narrative_index, current_conversation_index):
+        narrative_tree = self.get_narrative_tree()
+        if current_conversation_index + 1 < len(narrative_tree[str(current_narrative_index)]):
+            return current_narrative_index, current_conversation_index + 1
+        else:
+            return current_narrative_index + 1, 0
+
+    def save_reader_position(self, narrative_index, conversation_index):
+        with open(self.reader_position_fpath, 'w') as f:
+            json.dump((narrative_index, conversation_index), f)
--- test/test_fstore.py
+++ test/test_fstore.py
@@ -13,7 +13,7 @@ def test_init():
         with open(store.uid_index_fpath, 'r') as f:
             assert json.load(f) == {}
 
-def test_create_append_conversation():
+def test_filenarrative_workflow():
     test_uid1 = 'testuid'
     test_uid2 = 'anothertestuid'
 
@@ -23,13 +23,35 @@ def test_create_append_conversation():
         store.append_conversation_record(['hello, Alice!', 'hello, Bob!'])
 
         expected_fpath = os.path.join(tempdir, 
-                                      store.narrative_dir_format.format(idx=1, uid=test_uid1), 
+                                      store.narrative_dir_format.format(idx=0), 
                                       store.conversation_file_format.format(idx=0))
         with open(expected_fpath, 'r') as f:
             assert f.read() == 'hello, Alice!\nhello, Bob!\n'
 
         store.append_conversation_record(['heya, Charlie!', 'howdy, Dahlia!'])
         with open(expected_fpath, 'r') as f:
-            assert f.read() == 'hello, Alice!\nhello, Bob!\nheya, Charlie!\nhowdy, Dahlia!'
+            assert f.read() == 'hello, Alice!\nhello, Bob!\nheya, Charlie!\nhowdy, Dahlia!\n'
 
-        store.create_conversation_record(test_uid)
+        store.create_conversation_record(test_uid1)
+        store.append_conversation_record(['hello again, Alice!', 'hello again, Bob!', 'I am ghosting you!', 'END OF NARRATIVE'])
+        expected_fpath = os.path.join(tempdir, 
+                                      store.narrative_dir_format.format(idx=0), 
+                                      store.conversation_file_format.format(idx=1))
+        expected_output = 'hello again, Alice!\nhello again, Bob!\nI am ghosting you!\nEND OF NARRATIVE\n'
+        with open(expected_fpath, 'r') as f:
+            assert f.read() == 'hello again, Alice!\nhello again, Bob!\nI am ghosting you!\nEND OF NARRATIVE\n'
+        file_getter_output = store.get_conversation_record(store.get_set_narrative_idx(test_uid1), 1)
+        assert file_getter_output == expected_output
+        
+        store.create_conversation_record(test_uid2)
+        store.append_conversation_record(['hello, Anna!', 'hello, Billy!'])
+        expected_fpath = os.path.join(tempdir, 
+                                      store.narrative_dir_format.format(idx=1), 
+                                      store.conversation_file_format.format(idx=0))
+        with open(expected_fpath, 'r') as f:
+            assert f.read() == 'hello, Anna!\nhello, Billy!\n'
+
+        assert store.count_unread_conversations() == 2
+
+        assert store.get_next_conversation_index(0, 0) == (0, 1)
+        assert store.get_next_conversation_index(0, 1) == (1, 0)
